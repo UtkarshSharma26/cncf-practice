51fba92d20487a3e32a6644893f8b53896f4726c
UtkarshSharma26 <utkarsh.sharma26@hotmail.com>
2021-06-27 21:00:05 +0530

    Migrated from udacity--cncf

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask import json
+import logging
+import time
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/status")
+def status():
+    response = app.response_class(
+        response=json.dumps({"result": "OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+    app.logger.info('Status request successfull')
+    return response
+@app.route("/metrics")
+def metrics():
+    response = app.response_class(
+        response=json.dumps({"data": {"UserCount": 140, "UserCountActive": 23}, "status":"success", "code":0}),
+        status=200,
+        mimetype='application/json'
+    )
+    app.logger.info('Metrics request successfull')
+    return response
+  
+
+if __name__ == "__main__":
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
+    app.run(host='0.0.0.0')
